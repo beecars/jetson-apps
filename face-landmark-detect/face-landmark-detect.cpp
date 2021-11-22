06f15b1cc6dab0ffa3c7326edfbe9eff6b12b832
Brandon Carson <besidehimself@gmail.com>
2021-11-22 02:54:04 -0600

    added debug mode

--- face-landmark-detect/face-landmark-detect.cpp
+++ face-landmark-detect/face-landmark-detect.cpp
@@ -18,6 +18,14 @@
 #include <ctime>
 #include <string>
 using namespace cv;
+ 
+// ---- DEBUG MODE.
+/* 
+	Setting DEBUG to true will disable the camera stream, and instead use a local
+    image file as the network input.
+*/ 
+bool DEBUG = true;
+std::string DEBUG_image_path = "/home/beecars/Projects/jetson-apps/face-landmark-detect/debug_image.jpg";
 
 // ---- SETUP PARAMS. 
 float confThreshold = 0.5;
@@ -37,27 +45,32 @@ int main()
 		- IMX477 camera sensor (3rd party driver required).
 		Tuned for "aggresive" latency (downstream framerate not enforced, sink can drop frames).
     */
-	cv::Mat frame;	// Current frame Mat object.
+   	cv::Mat frame;	// Current frame Mat object.
+   	cv::VideoCapture cap;
 
-	std::string rx_gstream_pipe{};
-	rx_gstream_pipe = "nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)1280, height=(int)720, framerate = 30/1 ! nvvidconv flip-method=6 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink drop=true";
-	cv::VideoCapture cap(rx_gstream_pipe, CAP_GSTREAMER);
-	
-	// ---- INITIALIZE DNN MODULE FOR TINY-YOLOv4.
+	if (!DEBUG)		// If not DEBUG, assign videocapture object.
+	{
+		std::string rx_gstream_pipe{};
+		rx_gstream_pipe = "nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)1280, height=(int)720, framerate = 30/1 ! nvvidconv flip-method=6 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink drop=true";
+		cv::VideoCapture cap(rx_gstream_pipe, CAP_GSTREAMER);
+	}
+
+	// ---- INITIALIZE DNN MODULE.
 	/*
-		Custom face detection model from Darknet:
-		Trained on WIDER-FACE dataset for robust detection. 
+		Custom face detection model from YOLOv4-tiny trained on Darknet:
+		Used WIDER-FACE dataset for robust detection. 
 	*/
 	std::string yolo_face_cfg_Path{"/home/beecars/Projects/jetson-apps/face-landmark-detect/models/yolov4-tiny-face.cfg"};
 	std::string yolo_face_weights_Path{"/home/beecars/Projects/jetson-apps/face-landmark-detect/models/yolov4-tiny-face_final.weights"};
 	auto faceDetNet = dnn::readNetFromDarknet(yolo_face_cfg_Path, yolo_face_weights_Path);
 	faceDetNet.setPreferableBackend(dnn::DNN_BACKEND_CUDA);
 	faceDetNet.setPreferableTarget(dnn::DNN_TARGET_CUDA);
+	
 	// Get output blob layer names (for dnn::Net::forward()).
 	std::vector<std::string> outputBlobLayerNames = faceDetNet.getUnconnectedOutLayersNames();
 
 	// ---- STREAM PROCESSING.
-		
+
 	// Define variables for frametime measurement.
 	double fps{0.0};
 	double fps_array[10] = {};
@@ -69,13 +82,18 @@ int main()
 	std::cout << "Processing frames...\nPress 'Q' to terminate capture.\n";
 	for (;;)
 	{
+		frametime_start = std::chrono::system_clock::now();
+
 		if (waitKey(1) == 'q')	// End reading stream.
 		{
 			break;
 		}
-		frametime_start = std::chrono::system_clock::now();
-			
-		cap.read(frame);	// Read current frame from stream.
+		if (!DEBUG){
+			cap.read(frame);	// Read current frame from stream.
+		}
+		else {
+			frame = cv::imread(DEBUG_image_path);	// Read DEBUG image instead.
+		}
 
 		// ---- DETECT FACES
 		// Create blob for network input (RGB image).
@@ -86,17 +104,28 @@ int main()
 										   false, false);
 		// Set blob as network input.
 		faceDetNet.setInput(frameBlob);
+		
 		// Declare network output as blob (box coords, class names, confidences).
 		std::vector<cv::Mat> outputBlobs;
+		
 		// Feedforward input through network to get output blobs.
+		/* Details...
+			"outputBlobs" come from the YOLO layers of the YOLOv4-tiny network. 
+			Each blob has shape defined by:
+				# rows = # YOLO grid subsets for that layer. 
+				# cols = 5 + N (where N = # classes).
+			Column headers are:
+				| centerX | centerY | width | height | obj. score | class 0 conf. | ... | class N conf. |
+		*/
 		faceDetNet.forward(outputBlobs, outputBlobLayerNames);
+		
 		// Process and draw boxes on frame.
 		processBoxes(frame, outputBlobs);
 		
-		// ---- SHOW DETECTIONS.
+		// Show detections.
 		imshow("Live Camera Stream", frame);
 
-	// ---- PRINT FRAMERATE.
+	// Print framerate to console. 
 	frametime_end = std::chrono::system_clock::now();
 	elapsed_seconds = frametime_end - frametime_start;
 	fps = 1.0 / elapsed_seconds.count();
@@ -112,9 +141,9 @@ void processBoxes(cv::Mat& frame, const std::vector<cv::Mat>& outputBlobs)
 	// Iterate through all blobs output from the network. 
 	for (size_t i = 0; i < outputBlobs.size(); ++i)
 	{
-		// Get pointer for float data stored in outputBlobs.
+		// Get pointer for float data stored in <cv::Mat>outputBlobs.
 		float* data = (float*)outputBlobs[i].data;
-		// Iterate through each entry in the blob.  
+		// Iterate through each entry (row) in the <cv::Mat>outputBlob. 
 		for (int j = 0; j < outputBlobs[i].rows; ++j, data += outputBlobs[i].cols)
 		{
 			cv::Mat scores = outputBlobs[i].row(j).colRange(5, outputBlobs[i].cols);
